2b4ed80270f6a8201a46fd3cad954e2864953a0f
Diana Nitzschke <diddy-nit@web.de>
2023-05-24 20:20:12 +0200

    added conformal factor for one black hole

--- horizon.py
+++ horizon.py
@@ -5,4 +5,14 @@ method which solves a boundary value problem by reducing it to an initial
 value problem."""
 import numpy as np
 import matplotlib.pyplot as plt
+#Brill-Lindquist
 
+#One black hole 
+def fpsi(theta,r,M):
+    return 1+M/(2*r)
+
+def drfpsi(theta,r,M):
+    return -M/(2*r**2)
+
+def dthetafpsi(theta,r,M):
+    return 0
