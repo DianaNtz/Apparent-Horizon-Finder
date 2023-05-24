6e31c655b666955cf0a92bb7c77614d46d39da9f
Diana Nitzschke <diddy-nit@web.de>
2023-05-24 20:22:18 +0200

    added horizon equation in axi symmetry

--- horizon.py
+++ horizon.py
@@ -1,8 +1,10 @@
-"""The code below was written by @author: https://github.com/DianaNtz and
+"""
+The code below was written by @author: https://github.com/DianaNtz and
 solves the Apparent Horizon equation in axi symmetry for a time symmetric 
 conformally flat metric. The code is an implementation of the shooting 
 method which solves a boundary value problem by reducing it to an initial 
-value problem."""
+value problem.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 #Brill-Lindquist
@@ -16,3 +18,19 @@ def drfpsi(theta,r,M):
 
 def dthetafpsi(theta,r,M):
     return 0
+def fr1(r1,r2,theta,M):
+    return r2
+
+def fr2(r1,r2,theta,M):
+    
+    if(np.abs(theta) < 10**(-8) or np.abs(theta-np.pi) < 10**(-8) ):
+        factor=0
+    else:
+        factor=r2*(1+(r2/r1)**2)*(np.cos(theta)/np.sin(theta))
+        
+    value=2*r1-factor+4*r1**2*(1+(r2/r1)**2)/fpsi(theta,r1,M)*(drfpsi(theta,r1,M)
+         -(1/r1**2)*r2*dthetafpsi(theta,r1,M))+3*r2**2/r1
+    if(np.abs(r1) > 1.2182772912493089e+14 or np.abs(r1) < 10**(-8)):        
+        return 0
+    else:
+        return value
