6c2bad47d42e049e5f49a36f7521691a69d8f07a
Diana Nitzschke <diddy-nit@web.de>
2023-05-24 20:14:24 +0200

    added horizon.py

---
+++ horizon.py
@@ -0,0 +1,8 @@
+"""The code below was written by @author: https://github.com/DianaNtz and
+solves the Apparent Horizon equation in axi symmetry for a time symmetric 
+conformally flat metric. The code is an implementation of the shooting 
+method which solves a boundary value problem by reducing it to an initial 
+value problem."""
+import numpy as np
+import matplotlib.pyplot as plt
+
