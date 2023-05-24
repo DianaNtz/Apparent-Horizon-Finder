93387825b7222bd57b15acb310723489daa4af13
Diana Nitzschke <diddy-nit@web.de>
2023-05-24 20:45:37 +0200

    solving and plotting two black holes with different mass

--- horizon.py
+++ horizon.py
@@ -21,7 +21,7 @@ def dthetafpsi(theta,r,M):
     return 0
 """
 #two black holes with same mass
-
+"""
 def fpsi(theta,r,M):
     return 1+0.5*M/np.sqrt(r**2+2*0.75*r*np.cos(theta)+(0.75)**2)+0.5*M/np.sqrt(r**2-2*0.75*r*np.cos(theta)+(0.75)**2)
 
@@ -30,6 +30,17 @@ def drfpsi(theta,r,M):
 
 def dthetafpsi(theta,r,M):
     return 0.5*M/np.sqrt(r**2+2*0.75*r*np.cos(theta)+(0.75)**2)**3*(0.75*r*np.sin(theta))-0.5*M/np.sqrt(r**2-2*0.75*r*np.cos(theta)+(0.75)**2)**3*(0.75*r*np.sin(theta))
+"""
+#two black holes with different mass
+
+def fpsi(theta,r,M):
+    return 1+0.5*M*0.8/np.sqrt(r**2+2*0*r*np.cos(theta)+(0)**2)+0.5*M*0.2/np.sqrt(r**2-2*0.65*r*np.cos(theta)+(0.65)**2)
+
+def drfpsi(theta,r,M):
+    return -0.5*M*0.8/np.sqrt(r**2+2*0*r*np.cos(theta)+(0)**2)**3*(r+0*np.cos(theta))-0.5*M*0.2/np.sqrt(r**2-2*0.65*r*np.cos(theta)+(0.65)**2)**3*(r-0.65*np.cos(theta))
+
+def dthetafpsi(theta,r,M):
+    return 0.5*M*0.8/np.sqrt(r**2+2*0*r*np.cos(theta)+(0)**2)**3*(0*r*np.sin(theta))+0.5*M*0.2/np.sqrt(r**2-2*0.65*r*np.cos(theta)+(0.65)**2)**3*(-0.65*r*np.sin(theta))
 
 def fr1(r1,r2,theta,M):
     return r2
@@ -119,7 +130,8 @@ def find(ra1,ra2,M):
     return theta1*0,r1*0
 
 #theta,r=find(0.505,0.495,1)  #one black hole
-theta,r=find(1.27,1.28,1)  #two black holes with same mass
+#theta,r=find(1.27,1.28,1)  #two black holes with same mass
+theta,r=find(0.775588,0.776,1) #two black holes with different mass
 
 fig, ax = plt.subplots(figsize=(6,5))
 ax.plot(r*np.sin(theta),r*np.cos(theta),"-",linewidth=3.0,color='blue')
@@ -129,13 +141,18 @@ ax.plot(-r*np.sin(theta),r*np.cos(theta),":",linewidth=3.0,color='blue')
 #ax.set_xlim(-0.6,0.6)
 #ax.set_ylim(-0.6,0.6)
 #two black holes with same mass
-plt.plot(0, -0.75, marker="o", markersize=20, markerfacecolor="k")
-plt.plot(0, 0.75, marker="o", markersize=20, markerfacecolor="k")
-ax.set_xlim(-0.8,0.8)
-ax.set_ylim(-1.4,1.4)
+#plt.plot(0, -0.75, marker="o", markersize=20, markerfacecolor="k")
+#plt.plot(0, 0.75, marker="o", markersize=20, markerfacecolor="k")
+#ax.set_xlim(-0.8,0.8)
+#ax.set_ylim(-1.4,1.4)
+#two black holes with different mass
+plt.plot(0, 0.65, marker="o", markersize=10, markerfacecolor="k")
+plt.plot(0, 0, marker="o", markersize=40, markerfacecolor="k")
+ax.set_xlim(-0.5,0.5)
+ax.set_ylim(-0.5,0.8)
 plt.xlabel("x",fontsize=19) 
 plt.ylabel(r'z',fontsize=19,rotation=0)
 plt.xticks(fontsize= 14) 
 plt.yticks(fontsize= 14) 
-plt.savefig('figures/2blackholes.png',dpi=100)
+plt.savefig('figures/2blackholesdifferent.png',dpi=100)
 plt.show()   
