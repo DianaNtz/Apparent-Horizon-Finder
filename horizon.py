769d5b7bcf3c24340aa97c7cc9d31e1ad0710c13
Diana Nitzschke <diddy-nit@web.de>
2023-05-29 13:59:57 +0200

    finite difference derivatives

--- horizon.py
+++ horizon.py
@@ -6,3 +6,24 @@ method which solves an elliptic PDE by transforming it into a hyperbolic equatio
 """
 import numpy as np
 import matplotlib.pyplot as plt
+
+n=100
+
+def d2theta(D,theta):
+    dtheta=theta[1]-theta[0]
+    Dxx=np.zeros((n), dtype='double')
+    for j in range(0,n):
+                 if(j!=0 and j!=n-1):
+                     Dxx[j]=(D[j+1]-2*D[j]+D[j-1])/(dtheta**2)
+    Dxx[0]=(D[1]-2*D[0]+D[n-1])/(dtheta**2)
+    Dxx[n-1]=(D[0]-2*D[n-1]+D[n-2])/(dtheta**2)
+    return Dxx
+
+def d1theta(D,theta):
+    dtheta=theta[1]-theta[0]
+    Dx=np.zeros((n), dtype='double')
+    for j in range(0,n):
+                 if(j!=0 and j!=n-1):
+                     Dx[j]=(D[j+1]-D[j-1])/(2*dtheta)
+    return Dx  
+
