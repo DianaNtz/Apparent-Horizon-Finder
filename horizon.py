c71335d02ee60fbe3cfc939b30033e37c8ed33de
Diana Nitzschke <diddy-nit@web.de>
2023-05-31 18:11:41 +0200

    changed comment

--- horizon.py
+++ horizon.py
@@ -1,5 +1,5 @@
 """
-The code below was written by @author: https://github.com/DianaNtz and
+The code below was written by @author: Diana Nitzschke (https://github.com/DianaNtz) and
 solves the Apparent Horizon equation in axi symmetry for a time symmetric 
 conformally flat metric. The code is an implementation of the flow 
 method which solves an elliptic PDE by transforming it into a hyperbolic equation. 
